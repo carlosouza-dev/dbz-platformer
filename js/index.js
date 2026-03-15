03f41aa00dea16bf50479553de54f653b8000ca7
Carlos Henrique <carlosouza.dev@gmail.com>
2026-03-14 22:07:29 -0300

    feature: cenario e colisão

--- js/index.js
+++ js/index.js
@@ -9,18 +9,23 @@ function jump(){
 
     setTimeout(() => {
         persona.classList.remove('jump');
-        persona.src = "assets/images/goku-running.gif"
+        persona.src = "assets/images/goku-running.gif";
     }, 600);
 }
 
 const loop = setInterval(() => {
     const energyBallPosition = energyBall.offsetLeft;
-    const personaPosition = +window.getComputedStyle(persona).bottom.replace("px", "");
+    const personaPosition = Number(window.getComputedStyle(persona).bottom.replace("px", ""));
 
     console.log(personaPosition);
 
     if (energyBallPosition < 70 && energyBallPosition > 0 && personaPosition < 60){
         energyBall.style.animation = 'none';
         energyBall.style.left = "-50px";
+
+        persona.src = "assets/images/goku-dead.png";
+        persona.style.marginBottom = "-10px";
+
+        clearInterval(loop); 
     }
 }, 10)
