96f92b5eb08bfd80c3b62868f781b45afa31a916
Carlos Henrique Souza da Silva <carlosouza.dev@gmail.com>
2026-03-26 09:38:23 -0300

    attribute position

--- js/Persona.js
+++ js/Persona.js
@@ -4,8 +4,17 @@ class Persona {
 
     constructor(selector){
         this.element = document.querySelector(selector);
+        this.positionX = 0;
+        this.positionY = 0;
+
+        this.#initializePosition();
 
         this.#isDead = false;
+    }
+
+    #initializePosition() {
+        this.element.style.rigth = this.positionX;
+        this.element.style.bottom = this.positionY;
     }
 
     isDead() {
